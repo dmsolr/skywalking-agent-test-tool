d0e92667fd483dc756a0c6ecc0c69fbee7216bf4
daming <zteny@foxmail.com>
2020-04-04 19:31:02 +0800

    fix

--- mock-collector/src/main/java/org/apache/skywalking/plugin/test/mockcollector/mock/MockManagementService.java
+++ mock-collector/src/main/java/org/apache/skywalking/plugin/test/mockcollector/mock/MockManagementService.java
@@ -28,7 +28,7 @@ public class MockManagementService extends ManagementServiceGrpc.ManagementServi
 
     @Override
     public void reportInstanceProperties(InstanceProperties request, StreamObserver<Commands> responseObserver) {
-        responseObserver.onNext(Commands.getDefaultInstance());
+        responseObserver.onNext(Commands.newBuilder().build());
         responseObserver.onCompleted();
     }
 
